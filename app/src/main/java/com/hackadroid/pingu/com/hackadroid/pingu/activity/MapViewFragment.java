597c3bc2f812d1ca1fd5d1d9a796da4aa7a96f50
HarshithaSolai <solaiharshitha0@gmail.com>
2017-04-26 10:51:30 +0530

    fragment class

---
+++ app/src/main/java/com/hackadroid/pingu/com/hackadroid/pingu/activity/MapViewFragment.java
@@ -0,0 +1,138 @@
+package com.hackadroid.pingu.com.hackadroid.pingu.activity;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import android.app.Activity;
+
+import com.hackadroid.pingu.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MapViewFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MapViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapViewFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private OnFragmentInteractionListener mListener;
+public Button b1;
+
+    private static Context m=null;
+    public MapViewFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapViewFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapViewFragment newInstance(String param1, String param2) {
+                MapViewFragment fragment = new MapViewFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View r=inflater.inflate(R.layout.fragment_map_view, container, false);
+
+
+        b1 = (Button)r.findViewById(R.id.button2);
+        this.b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                checkConnection();
+
+            }
+        });
+        return r;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private void checkConnection() {
+
+
+        final LocationManager manager = (LocationManager)m.getSystemService(Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) )
+            Toast.makeText(m, "GPS is disabled!", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(m, "GPS is enabled!", Toast.LENGTH_LONG).show();
+}
+}
